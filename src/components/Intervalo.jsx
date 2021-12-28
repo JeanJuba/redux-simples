ff6fe0e7baf48988790804360818a191a214b386
JeanJuba <jeanwsj96@hotmail.com>
2021-12-28 17:44:20 -0300

    Antes aula logica

---
+++ src/components/Intervalo.jsx
@@ -0,0 +1,21 @@
+import './Intervalo.css'
+import React from 'react'
+import Card from './Card'
+
+export default props => {
+    return (
+        <Card title="Intevralo de Números" red>
+            <div className='Intervalo'>
+                <span>
+                    <strong>Mínimo:</strong>
+                    <input type="number" value={0}></input>
+                </span>
+                <span>
+                    <strong>Máximo:</strong>
+                    <input type="number" value={10}></input>
+                </span>
+            </div>
+
+        </Card>
+    )
+}
