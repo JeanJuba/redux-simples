f76142c8443b9bb6976e9b997b0ada5a3e452139
JeanJuba <jeanwsj96@hotmail.com>
2021-12-29 08:49:34 -0300

    Concluir projeto com sistema de estados simples

--- src/components/Intervalo.jsx
+++ src/components/Intervalo.jsx
@@ -3,16 +3,17 @@ import React from 'react'
 import Card from './Card'
 
 export default props => {
+    const { min, max } = props;
     return (
         <Card title="Intevralo de Números" red>
             <div className='Intervalo'>
                 <span>
                     <strong>Mínimo:</strong>
-                    <input type="number" value={0}></input>
+                    <input type="number" value={min} onChange={e => props.onMinChanged(+e.target.value)}></input>
                 </span>
                 <span>
                     <strong>Máximo:</strong>
-                    <input type="number" value={10}></input>
+                    <input type="number" value={max} onChange={e => props.onMaxChanged(+e.target.value)}></input>
                 </span>
             </div>
 
