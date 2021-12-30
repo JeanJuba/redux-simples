66e3c550e9d60dcfb00e6afcb1b442cec279ae5f
JeanJuba <jeanwsj96@hotmail.com>
2021-12-30 17:22:49 -0300

    Concluir aplicacao

--- src/store/actions/numeros.js
+++ src/store/actions/numeros.js
@@ -1,16 +1,19 @@
-import {} from  './actionTypes'
+import {
+    NUM_MIN_ALTERADO,
+    NUM_MAX_ALTERADO
+} from './actionTypes'
 
 // Action Creator
-export function alterarNumeroMinimo(novoNumero){
+export function alterarNumeroMinimo(novoNumero) {
     return {
-        type: 'NUM_MIN_ALTERADO',
+        type: NUM_MIN_ALTERADO,
         payload: novoNumero
     }
 }
 
-export function alterarNumeroMaximo(novoNumero){
+export function alterarNumeroMaximo(novoNumero) {
     return {
-        type: 'NUM_MAX_ALTERADO',
+        type: NUM_MAX_ALTERADO,
         payload: novoNumero
     }
 }
